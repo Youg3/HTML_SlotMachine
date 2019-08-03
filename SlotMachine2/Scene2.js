000078c2bdb40ac076d65086ba3bd58ae062748f
Joshua Astbury <j.astbuy@ymail.com>
2019-08-03 11:21:09 +0200

    Spritesheets tut5
    Tutorial 5 using spritesheets

--- SlotMachine2/Scene2.js
+++ SlotMachine2/Scene2.js
@@ -14,10 +14,42 @@ class Scene2 extends Phaser.Scene
 		this.background.setOrigin(0, 0); //reset background origin point to top left from centre
 
 		//add ships using the global config var to position the ships
-		this.ships1 = this.add.image(config.width /2 - 50, config.height /2, "ship");
-		this.ships2 = this.add.image(config.width /2, config.height /2, "ship2");
-		this.ships3 = this.add.image(config.width /2 + 50, config.height /2, "ship3");
+		this.ship1 = this.add.sprite(config.width /2 - 50, config.height /2, "ship").setInteractive();
+		this.ship2 = this.add.sprite(config.width /2, config.height /2, "ship2").setInteractive();
+		this.ship3 = this.add.sprite(config.width /2 + 50, config.height /2, "ship3").setInteractive();
 
+		//animations
+		this.anims.create({
+			key: "ship1_anim",
+			frames: this.anims.generateFrameNumbers("ship"),
+			frameRate: 20,
+			repeat: -1 //repeats indefinitely
+		});
+		this.anims.create({
+			key: "ship2_anim",
+			frames: this.anims.generateFrameNumbers("ship2"),
+			frameRate: 20,
+			repeat: -1
+		});
+		this.anims.create({
+			key: "ship3_anim",
+			frames: this.anims.generateFrameNumbers("ship3"),
+			frameRate: 20,
+			repeat: -1
+		});
+		this.anims.create({
+			key: "explode",
+			frames: this.anims.generateFrameNumbers("explosion"),
+			frameRate: 20,
+			repeat: 0, //doesn't repeat unless called
+			hideOnComplete: true
+		});
+
+		this.ship1.play("ship1_anim");
+		this.ship2.play("ship2_anim");
+		this.ship3.play("ship3_anim");
+
+		this.input.on('gameobjectdown', this.destroyShip, this);
 
 		this.add.text(20, 20, "Game loaded...", {font: "25px Arial", fill: "yellow"});
 	}
@@ -38,10 +70,15 @@ class Scene2 extends Phaser.Scene
 		ship.x = randomX;
 	}
 
+	destroyShip(pointer, gameObject) {
+		gameObject.setTexture("explosion");
+		gameObject.play("explode");
+	}
+
 	update() {
-		this.moveShip(this.ships1, 1);
-		this.moveShip(this.ships2, 2);
-		this.moveShip(this.ships3, 3);
+		this.moveShip(this.ship1, 1);
+		this.moveShip(this.ship2, 2);
+		this.moveShip(this.ship3, 3);
 
 		//decrease Y position of the background texture map so it scrolls down
 		this.background.tilePositionY -= 0.5;
