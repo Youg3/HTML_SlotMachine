296ddbdee40cdeafc807e24b48ac108cf84d7902
Joshua Astbury <j.astbuy@ymail.com>
2019-08-03 10:52:00 +0200

    Scrolling Background and Ship Movement
    tutorial part 4

--- SlotMachine2/Scene2.js
+++ SlotMachine2/Scene2.js
@@ -9,8 +9,8 @@ class Scene2 extends Phaser.Scene
 
 	create() 
 	{
-		//set background
-		this.background = this.add.image(0, 0, "background");
+		//set background as a tilesprite to allow for scrolling
+		this.background = this.add.tileSprite(0, 0, config.width, config.height, "background");
 		this.background.setOrigin(0, 0); //reset background origin point to top left from centre
 
 		//add ships using the global config var to position the ships
@@ -20,6 +20,31 @@ class Scene2 extends Phaser.Scene
 
 
 		this.add.text(20, 20, "Game loaded...", {font: "25px Arial", fill: "yellow"});
+	}
+
+	moveShip(ship, speed) 
+	{
+		ship.y += speed; //ship velocity
+
+		if (ship.y > config.height) //resets the y position if off screen
+		{
+			this.resetShipPos(ship);
+		}
+	}
+
+	resetShipPos(ship) {
+		ship.y = 0;
+		var randomX = Phaser.Math.Between(0, config.width);
+		ship.x = randomX;
+	}
+
+	update() {
+		this.moveShip(this.ships1, 1);
+		this.moveShip(this.ships2, 2);
+		this.moveShip(this.ships3, 3);
+
+		//decrease Y position of the background texture map so it scrolls down
+		this.background.tilePositionY -= 0.5;
 	}
 
 }
